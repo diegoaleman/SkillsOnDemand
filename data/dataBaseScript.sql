b04901631df8fd7002ff62a577f63871c7de21c0
Jorge Luis Perales <jorgelp94@MacBook-Pro-de-Jorge.local>
2015-11-12 14:34:28 -0600

    sql

--- data/dataBaseScript.sql
+++ data/dataBaseScript.sql
@@ -9,3 +9,37 @@ CREATE TABLE Client (
 INSERT INTO Client(fName, lName, email, passwrd)
 VALUES  ('Jorge', 'Perales', 'jorgelp94', 'test'),
 		('Diego', 'Aleman', 'diegoaleman', 'test');
+
+CREATE TABLE Cart(
+	orderId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(250) NOT NULL,
+	skill VARCHAR(30) NOT NULL,
+	quantity INT NOT NULL,
+	status CHAR(1) NOT NULL,
+	FOREIGN KEY (email) 
+		REFERENCES Client (email)
+		ON DELETE CASCADE,
+	FOREIGN KEY (skill) 
+		REFERENCES Skill (skillId)
+		ON DELETE CASCADE
+);
+
+CREATE TABLE Skill(
+	skillId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	title VARCHAR(250) NOT NULL,
+	description VARCHAR(250) NOT NULL,
+	email VARCHAR(250) NOT NULL,
+	categoryId VARCHAR(250) NOT NULL,
+	FOREIGN KEY (email)
+		REFERENCES Client (email)
+		ON DELETE CASCADE,
+	FOREIGN KEY (categoryId)
+		REFERENCES Category (categoryId)
+		ON DELETE CASCADE
+);
+
+CREATE TABLE Category(
+	categoryId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	title VARCHAR(250) NOT NULL,
+	description VARCHAR(250) NOT NULL
+);
